b26d07089cb66281ec6097fa7747e660d2e4c0bc
Your Name <you@example.com>
2018-11-15 14:14:35 -0700

    Initial Commit

---
+++ phidgetsRepeat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  phidgetsRepeat
+//
+//  Created by Lovet Nguatem on 2018-11-15.
+//  Copyright © 2018 Lovet Nguatem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
